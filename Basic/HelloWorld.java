ce4491936d9948bde707ef8910fade74f9d80cde
deepakkumar1997 <pandeydeepak.kumar1997@gmail.com>
2022-11-03 00:49:09 +0530

    basic of java program

---
+++ Basic/HelloWorld.java
@@ -0,0 +1,16 @@
+package Basic;
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+// public-- Access specifier, here it is public so main function can be accessed from
+// outside of the class;
+// static-- Here main function is a static function so it can be called without creating
+// an object of HelloWorld class;
+// void-- return type
+
+// System-- is a class
+// out-- reference variable or indirectly an object 
+// println()-- a method to print anything to console;
