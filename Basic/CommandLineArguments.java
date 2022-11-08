bc8188248d18ffeec3a31656d41c4b718e881dc5
deepakkumar1997 <pandeydeepak.kumar1997@gmail.com>
2022-11-09 02:05:05 +0530

    Command line arguments in java

---
+++ Basic/CommandLineArguments.java
@@ -0,0 +1,8 @@
+public class CommandLineArguments {
+    public static void main(String []args){
+        // Print all command line arguments
+        for(String string : args){
+            System.out.println(string);
+        }
+    }
+}
