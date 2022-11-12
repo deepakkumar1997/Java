fe5b1fe1bcdbc263ab7256bee665174808c0059c
deepakkumar1997 <pandeydeepak.kumar1997@gmail.com>
2022-11-12 16:00:26 +0530

    Command line args and array sum

--- Basic/CommandLineArguments.java
+++ Basic/CommandLineArguments.java
@@ -1,8 +1,14 @@
 public class CommandLineArguments {
     public static void main(String []args){
         // Print all command line arguments
-        for(String string : args){
+        for(String string : args)
             System.out.println(string);
+        // Sum of array elements
+        int numbers[] = {10,20,30,40,50};
+        int sum=0;
+        for(int i=0; i<numbers.length; ++i){
+            sum = sum + numbers[i];
         }
+        System.out.println("Sum of array elements are "+sum);
     }
 }
